be25a482978ec1f8a255e40d73591a0e6e2600df
methodXD <jstalberger@gmail.com>
2016-12-27 18:59:27 -0800

    Cleanup

--- plot3.R
+++ plot3.R
@@ -9,22 +9,18 @@ unzip("./exdata_data_household_power_consumption.zip", exdir = ".")
 # Read relevant data set into R
 data <- read.table("./household_power_consumption.txt", sep=";", 
                    header = TRUE, na.strings = "?")
-
-# Subset and merge data
-dataA <- subset(data,Date=="1/2/2007")
-dataB <- subset(data,Date=="2/2/2007")
-dataC <- rbind(dataA,dataB)
+# Subset data
+data <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
 
 # Convert Dates and Times and organize data
-Date2 <- as.Date(dataC$Date, "%d/%m/%Y")
-Time2 <- strptime(dataC$Time, format="%T")
-Time2 <- strftime(Time2, "%T")
-DateTime <- strptime(paste(Date2, Time2), format="%Y-%m-%d %H:%M:%S")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data$Time <- strftime(strptime(data$Time,format="%T"), "%T")
+DateTime <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
 DayOfWeek <- strftime(DateTime, format="%a")
-data2 <- cbind(DateTime, Date2, Time2, DayOfWeek, dataC[,c(3,4,5,6,7,8,9)])
+data2 <- cbind(DateTime, data$Date, data$Time, DayOfWeek, data[,c(3,4,5,6,7,8,9)])
 
 # Create PNG file device
-png(filename="plot3.png", width=480, height=480, bg="transparent")
+png(filename="plot3.png", width=480, height=480)
 
 # Create plot
 plot(data2$Sub_metering_1 ~ data2$DateTime,pch=NA,xlab="",
